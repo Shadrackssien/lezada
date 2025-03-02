863ab0db6cc81cfc2a9dff9a66ca026b353f1762
Shadrack Essien <shadrack6960@gmail.com>
2025-03-02 15:25:09 +0000

    Auth page ready

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,3 +1,41 @@
 <template>
-  <div>Login</div>
+  <div>
+    <div class="w-1/2 mx-auto my-24 bg-[#f7f7f7] space-y-8 p-8">
+      <div class="flex flex-col items-center justify-center">
+        <h2 class="text-center text-3xl tracking-wider">Login</h2>
+        <p class="text-[#7e7e7e] mt-4">Great to have you back</p>
+      </div>
+      <form @submit.prevent="login" class="flex flex-col gap-10 mt-4 w-full">
+        <input
+          class="outline-none border-b-2 py-2 border-b-gray-300"
+          type="text"
+          placeholder="Username or email address"
+        />
+        <input
+          class="outline-none border-b-2 py-2 border-b-gray-300"
+          type="password"
+          placeholder="Password"
+        />
+        <button
+          type="submit"
+          class="bg-[#333] border border-[#333] text-white py-2 uppercase w-32 hover:bg-transparent hover:text-black transition-all duration-300 cursor-pointer"
+        >
+          Login
+        </button>
+      </form>
+      <p class="text-[#7e7e7e]">
+        Don't have an account?
+        <router-link class="hover:underline hover:text-black" to="/signup"
+          >Sign up</router-link
+        >
+      </p>
+      <div class="flex items-center gap-1.5">
+        <input class="mt-1" type="checkbox" v-model="rememberMe" />
+        <span>Remember Me</span>
+      </div>
+      <div class="">
+        <a href="#">Lost your Password?</a>
+      </div>
+    </div>
+  </div>
 </template>
--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,3 +1,48 @@
+<script setup lang="ts">
+import { register } from "swiper/element";
+</script>
+
 <template>
-  <div>Register</div>
+  <div>
+    <div class="w-1/2 mx-auto my-24 space-y-8 p-8">
+      <div class="flex flex-col items-center justify-center">
+        <h2 class="text-center text-3xl tracking-wider">Register</h2>
+        <p class="text-[#7e7e7e] mt-4">
+          If you don't have an account, register now!
+        </p>
+      </div>
+      <form @submit.prevent="register" class="flex flex-col gap-10 mt-4 w-full">
+        <div class="flex flex-col">
+          <label for="email" class="uppercase tracking-wider"
+            >Email Address <span class="text-red-500">*</span></label
+          >
+          <input
+            class="outline-none border-b-2 py-2 border-b-gray-300"
+            type="email"
+          />
+        </div>
+        <div class="flex flex-col">
+          <label for="password" class="uppercase tracking-wider"
+            >Password <span class="text-red-500">*</span></label
+          >
+          <input
+            class="outline-none border-b-2 py-2 border-b-gray-300"
+            type="password"
+          />
+        </div>
+        <button
+          type="submit"
+          class="bg-[#333] border border-[#333] text-white py-2 uppercase w-32 hover:bg-transparent hover:text-black transition-all duration-300 cursor-pointer"
+        >
+          Register
+        </button>
+      </form>
+      <p class="text-[#7e7e7e]">
+        Already have an account?
+        <router-link class="hover:underline hover:text-black" to="/login"
+          >Login</router-link
+        >
+      </p>
+    </div>
+  </div>
 </template>
